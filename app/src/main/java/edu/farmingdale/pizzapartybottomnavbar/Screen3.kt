f8b9b677638c3db216c5863e1881b3cddb413ce1
Gagan <gagansapkota668@gmail.com>
2024-10-17 14:55:13 -0400

    Completed Todo3

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
@@ -35,9 +35,11 @@ fun Screen3() {
     var sliderValue by remember { mutableStateOf(0.5f) }
     var chkd by remember { mutableStateOf(true) }
 
+    val brushColor = arrayOf(0.0f to Color.White, 0.2f to Color.LightGray, 1f to Color.White)
 
     val context = LocalContext.current
-    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize(),
+    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize()
+        .background(Brush.verticalGradient(colorStops = brushColor)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
         Slider(value = sliderValue, onValueChange = { sliderValue=it }, Modifier.fillMaxWidth()
